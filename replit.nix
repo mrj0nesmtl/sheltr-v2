e74f744e6a00f595ad8d9c1e624643a774f7e8ac
Joel <joel.yaffe@gmail.com>
2025-01-20 23:36:11 -0500

    chore: consolidate replit configuration files and fix Node.js setup

--- replit.nix
+++ replit.nix
@@ -1,9 +1,16 @@
 { pkgs }: {
   deps = [
     pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
     pkgs.nodePackages.npm
+    pkgs.git
+    pkgs.replitPackages.jest
   ];
   env = {
     PATH = "${pkgs.nodejs-18_x}/bin:${pkgs.nodePackages.npm}/bin:$PATH";
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libuuid
+    ];
   };
 } 
